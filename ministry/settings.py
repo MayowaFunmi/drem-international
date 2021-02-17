24081fe95e463fa74404a8b646afea86795b5add
Akinade Mayowa Emmnauel <akinade.mayowa@gmail.com>
2021-02-17 20:47:32 +0100

    Update settings.py

--- ministry/settings.py
+++ ministry/settings.py
@@ -11,13 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
+# from decouple import config
 
-import environ
-
-env = environ.Env()
-# reading .env file
-environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
